3f5c08e20a4f7e0d98add63d499b25384e778eab
Degeur <90846136+Degeur@users.noreply.github.com>
2022-11-29 14:10:51 +0100

    prettier

--- script.py
+++ script.py
@@ -1,11 +1,7 @@
 import csv
 import random
+dico_couleur={0: "\033[34m", 1 : "\033[31m", 2 : "\033[32m", 3 : "\033[35m", 4 : "\033[39m", 5: "\033[34m", 6 : "\033[31m", 7 : "\033[32m", 8 : "\033[35m", 9 : "\033[39m"}
 
-class color:
-    g = '\033[92m' # vert
-    y = '\033[93m' # jaune
-    r = '\033[91m' # rouge
-    n = '\033[0m' #gris, couleur normale
 
 with open('abilities.csv', newline='') as fichier:
     lecture = csv.reader(fichier, delimiter=',')
